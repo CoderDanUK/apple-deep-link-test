985909e2b66fc8564419083446b1949ac83fddec
Dan Scott <Dan.Scott@sse.com>
2021-10-27 10:14:15 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+const port = 443
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+const getAppleAppSiteAssociation = () => {
+    const appleAppSiteAssociation = fs.readFileSync(path.join(__dirname, 'static/apple-app-site-association.json'), (err, data) => {
+        console.log('reading file')
+        if (err) {
+            console.error(err)
+            return `{"error": "${err}""}`
+        }
+        else {
+            return data
+        }
+    })
+
+    return appleAppSiteAssociation
+}
+
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    const json = JSON.parse(getAppleAppSiteAssociation())
+    res.json(json)
+})
+
+app.get('/', (req, res) => {
+    res.render('index', {openingApp: false})
+})
+
+app.get('/your-account/', (req, res) => {
+    res.render('index', {openingApp: true})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at https://localhost:${port}`)
+})
